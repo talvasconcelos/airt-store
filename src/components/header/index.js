f02a1c542bc03c56dda359bfc48588eecd4e22ba
Tiago Vasconcelos <tvasconcelos@sedeprosp.pt>
2019-06-27 17:39:59 +0100

    initial

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,19 @@
 import { h } from 'preact';
 import { Link } from 'preact-router/match';
-import style from './style';
 
 const Header = () => (
-	<header class={style.header}>
-		<h1>Preact App</h1>
+	<header>
+		<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+			<div class="navbar-brand">
+				AIRT
+			</div>
+		</nav>
+		{/* <h1>Preact App</h1>
 		<nav>
 			<Link activeClassName={style.active} href="/">Home</Link>
 			<Link activeClassName={style.active} href="/profile">Me</Link>
 			<Link activeClassName={style.active} href="/profile/john">John</Link>
-		</nav>
+		</nav> */}
 	</header>
 );
 
